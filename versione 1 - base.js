03e3a8d80520d608c14652a67a5a73fdabbf8c4a
sergioscarale <sergio.scarale11@gmail.com>
2024-05-09 16:05:49 +0200

    Menù + registrazione dati anagrafici ()
    Implementazione del menù e della prima funzione r_dati()

--- versione 1 - base.js
+++ versione 1 - base.js
@@ -4,5 +4,49 @@ Versione 1 - Base
 - La registrazione delle singole gare e dei relativi partecipanti.
 - La creazione ed aggiornamento della classifica di campionato.
 */
+const prompt=require("prompt-sync")();
 
-console.log("Hello World!");
+function r_dati()
+{
+    let partecipante=
+    {
+        nome: prompt("Nome >> "),
+        cognome: prompt("Cognome >> "),
+        sesso: prompt("Sesso >> "),
+        data: prompt("Data di nascita **/**/**** >> ")
+    };
+    
+}
+
+function r_gare()
+{
+
+}
+
+function classifica()
+{
+
+}
+
+function main()
+{
+    let scelta;
+    console.log("Calcolatore di statistiche campionato di atletica leggera\n");
+    do
+    {
+        console.log("VERSIONE 1 - Base");
+        console.log("0 - Esci;")
+        console.log("1 - Registrazione dei principali dati anagrafici dei giocatori;");
+        console.log("2 - Registrazione delle singole gare e dei relativi partecipanti;");
+        console.log("3 - Creazione ed aggiornamento della classifica di campionato.");
+        scelta=parseInt(prompt(">> "));
+        switch(scelta)
+        {
+            case 1: r_dati();       break;
+            case 2: r_gare();       break;
+            case 3: classifica();   break;
+        }
+    }while(scelta!==0);
+}
+
+main();
