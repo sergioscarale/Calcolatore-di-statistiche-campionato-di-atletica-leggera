30b02590be2647bba5083d2b7d207ef9d31b2bd3
dadu08 <davide.cocomazzi.libbero@gmail.com>
2024-05-13 17:22:54 +0200

    Funzioni utili per le gare
    Implementazione quasi completa della funziona r_gare() + perfezionamento. Funzione cento(), duecento(), stampaGara(). - Implementare funzione classifica.

--- versione 1 - base.js
+++ versione 1 - base.js
@@ -1,6 +1,6 @@
 /*
 Versione 1 - Base
-- La registrazione dei principali dati anagrafici dei giocatori.
+- La registrazione dei principali dati anagrafici dei giocatori.        👏
 - La registrazione delle singole gare e dei relativi partecipanti.
 - La creazione ed aggiornamento della classifica di campionato.
 */
@@ -18,14 +18,20 @@ class Partecipante
     }
 }
 
-function r_dati(partecipante_set)
+class Gara
 {
-    let nome=prompt("Nome: ");
-    let cognome=prompt("Cognome: ");
+    constructor(tipo_gara,tempo,partecipante)
+    {
+        this.tipo_gara=tipo_gara;
+        this.tempo=tempo;
+        this.partecipante=partecipante;
+    }
+}
+
+function controllo(partecipante_set,nome,cognome)
+{
     let trovato=false;
-
     //confrontare nome e cognome (?) per vedere se il partecipante è già iscritto
-
     partecipante_set.forEach(partecipante=>
         {
             if(partecipante.nome===nome && partecipante.cognome===cognome)
@@ -33,6 +39,14 @@ function r_dati(partecipante_set)
                 trovato=true;
             }
         });
+    return trovato;
+}
+
+function r_dati(partecipante_set)
+{
+    let nome=prompt("Nome: ");
+    let cognome=prompt("Cognome: ");
+    let trovato=controllo(partecipante_set,nome,cognome);
 
     if(!trovato)
     {
@@ -49,20 +63,85 @@ function r_dati(partecipante_set)
     }
 }
 
-function r_gare()
+function casuale()
 {
+    tempo=9.5+Math.random()*(11-9.5);
+    return tempo;
+}
 
+function cento(gara_set,nome,cognome)
+{
+    let tempo=casuale();
+    let partecipante=new Partecipante(nome,cognome);
+    let gara=new Gara("100 metri",tempo,partecipante);
+    gara_set.add(gara);
+    stampaGara(gara);
+}
+
+function duecento(gara_set,nome,cognome)
+{
+    let tempo=casuale();
+    let partecipante=new Partecipante(nome,cognome);
+    let gara=new Gara("200 metri",tempo,partecipante);
+    gara_set.add(gara);
+    stampaGara(gara);
+}
+
+function stampaGara(gara)
+ {
+    console.log("Tipo di gara: ",gara.tipo_gara);
+    console.log("Tempo: ",gara.tempo+" secondi");
+    console.log("Partecipante: ",gara.partecipante.nome,gara.partecipante.cognome);
+}
+
+function r_gare(partecipante_set,gara_set)
+{
+    let scelta,trovato_partecipante,trovato_gara;
+    do
+    {
+        console.log("Inserisci i partecipanti della gara");
+        let nome=prompt("Nome: ");
+        let cognome=prompt("Cognome: ");
+        trovato_partecipante=controllo(partecipante_set,nome,cognome);
+        for(let gara of gara_set)
+        {
+            if(gara.partecipante.nome===nome && gara.partecipante.cognome===cognome)
+            {
+                trovato_gara = true;
+                break;
+            }
+        }
+        if(trovato_partecipante===true && !trovato_gara)
+        {
+            console.log("\nTIPO DI GARA\n");
+            console.log("0 - Esci");
+            console.log("1 - 100 metri");
+            console.log("2 - 200 metri");
+            scelta=parseInt(prompt(">> "));
+            switch(scelta)
+            {
+                case 1: cento(gara_set,nome,cognome);        break;
+                case 2: duecento(gara_set,nome,cognome);     break;
+            }
+        }
+        else
+        {
+            console.log("Partecipante non presente o già registrato in una gara!\n");
+        }
+        scelta=parseInt(prompt("\n1 - Continua inserimento\n2 - Esci inserimento \n>> "));
+    }while(scelta!==2);
 }
 
 function classifica()
 {
-
+    //implementazione classifica
 }
 
 function main()
 {
     let scelta;
     let partecipante_set=new Set();
+    let gara_set=new Set();
     console.log("Calcolatore di statistiche campionato di atletica leggera\n");
     do
     {
@@ -74,8 +153,8 @@ function main()
         scelta=parseInt(prompt(">> "));
         switch(scelta)
         {
-            case 1: r_dati(partecipante_set);       break;
-            case 2: r_gare();       break;
+            case 1: r_dati(partecipante_set);               break;
+            case 2: r_gare(partecipante_set,gara_set);      break;
             case 3: classifica();   break;
         }
     }while(scelta!==0);
